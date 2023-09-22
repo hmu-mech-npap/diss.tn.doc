7c706a452e7d128bb9dc0c1a9e3a729d33893ede
Nick Torosian <goodvibrations32@protonmail.com>
2023-09-22 14:47:27 +0300

    adding all src files needed for latest comparison plots

---
+++ diss-docs/main_doc/src/barAndWiskr.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env ipython
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, tf2sos, sosfilt, firwin
+from data import noise_chan, clean_chan_off_0, fs_noise
+
+# zero speed graph
+noise_on_std = np.std(noise_chan.data)
+clean_off_std = np.std(clean_chan_off_0)
+
+
+fir_filt_coeff = firwin(numtaps=22,
+                        fs=fs_noise,
+                        cutoff=200,
+                        )
+sos_fir_mode = tf2sos(fir_filt_coeff, 1)
+filt_on_fir_std = sosfilt(sos_fir_mode, noise_chan.data)
+
+
+b, a = butter(2,
+              200,   # This is in Hz !!!!
+              # [min(noise_frequencies[L]), max(noise_frequencies[L]-1)],
+              btype="low", fs=fs_noise)
+
+# Apply filter
+sos_out = tf2sos(b, a)
+filtered_signal = sosfilt(sos_out, noise_chan.data)
+filt_on_iir_std = np.std(filtered_signal)
+# function to add value labels
+
+# Bar graph:
+#       Adding the comparison for standard deviation to backup my filter
+#       strategy.
+x_axis = ['Inverter Off', 'Inverter On',
+          'Filtered IIR On \n(order 2)', 'Filtered FIR On \n(order 22)']
+y_axis = [clean_off_std, noise_on_std, filt_on_iir_std, np.std(filt_on_fir_std)]
+labels = ["raw", "raw", "order 2", "order 22"]
+
+plt.title("Comparison for Wind Tunnel measurements and inverter On and Off")
+plt.xlabel("O m/s airflow")
+plt.ylabel("standard deviation")
+plt.bar(x_axis, y_axis)
+plt.show()
+
+# %%
+"""
+# TODO Explain the wisker plot in the document
+
+The documentation says:
+
+
+            Q2-1.5IQR   Q1   median  Q3   Q3+1.5IQR
+                         |-----:-----|
+         o      |--------|     :     |--------|  o  o
+                         |-----:-----|
+     lier                <----------->          fliers
+                              IQR
+
+- autorangebool, default: False
+    When True and the data are distributed such that the 25th and 75th
+    percentiles are equal, whis is set to (0, 100) such that the whisker ends
+    are at the minimum and maximum of the data.
+
+*reference* :
+    - https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.boxplot.html
+"""
+labels = ["Inverter Off",
+          "Inverter On",
+          "Butterworth low-pass\nat 200Hz(IIR)",
+          "Simple window low-pass\nat 200Hz(FIR)"]
+
+plt.boxplot([clean_chan_off_0, noise_chan, filtered_signal, filt_on_fir_std],
+            labels=labels,
+            # notch=True,
+            vert=True,
+            patch_artist=False,
+            meanline=True,
+            showmeans=True,
+            autorange=True,
+            showfliers=False
+            )
+plt.title("Wind Tunnel measurements with airflow O m/s")
+plt.xlabel("Filtering technics")
+plt.ylabel("Mean value of recorded signal")
+# plt.ylim(1.52, 1.72)
+plt.show()
---
+++ diss-docs/main_doc/src/time_dom_demo.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from scipy.signal import butter, tf2sos, sosfilt, firwin
+from data import (noise_chan, clean_signal,
+                  fs_noise, t_noise, t_clean, fs_clean)
+
+
+# FFT
+# frequencies_clean = np.fft.fftfreq(len(clean_signal), T_clean)
+# frequencies_noisy = np.fft.fftfreq(len(noise_chan.data), T_noise)
+# fft_clean = fft(clean_signal)
+# fft_noisy = fft(noise_chan.data)
+
+# FIR filter
+fir_filt_coeff = firwin(numtaps=22,
+                        fs=fs_noise,
+                        cutoff=200,
+                        )
+sos_fir_mode = tf2sos(fir_filt_coeff, 1)
+filt_on_fir_std = sosfilt(sos_fir_mode, noise_chan.data)
+
+
+# Butterworth filter design
+""" low pass is more suitable for us """
+b, a = butter(4,
+              200,   # This is in Hz !!!!
+              # [min(noise_frequencies[L]), max(noise_frequencies[L]-1)],
+              btype="low", fs=fs_clean)
+
+# Apply filter
+sos_out = tf2sos(b, a)
+filtered_signal = sosfilt(sos_out, noise_chan.data)
+
+# # Validation
+fig, axs = plt.subplots(3, 2)
+plot2 = plt.subplot2grid((3, 2), (1, 0), colspan=2)
+plot3 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
+plot2.plot(t_noise, noise_chan.data, label="Inverter On 0 m/s")
+plot2.plot(t_clean, clean_signal, label="Inverter Off 0 m/s")
+plot2.legend()
+axs[0, 0].set_title('Inverter On 0 m/s (FIR at 200 Hz)')
+axs[0, 0].plot(t_noise, filt_on_fir_std)
+axs[0, 1].set_title("Inverter On 0 m/s (IIR at 200 Hz)")
+axs[0, 1].plot(t_noise, filtered_signal)
+# axs[0, 1].set_title('Filtered signals with IIR and FIR')
+axs[0, 0].set_ylim([1.5, 1.75])
+axs[0, 1].set_ylim([1.5, 1.75])
+# axs[0, 1] = fig.add_gridspec(2, 2, hspace=0, wspace=0).subplots( sharey='row')
+# plot2.plot(t_noise, noise_chan.data, label="Inverter On 0 m/s")
+# plot3.plot(t_noise, noise_chan.data, label="Inverter On 0 m/s")
+# axs[1, 0].set_title('Axis [1, 0]')
+# axs[1, 1].plot(x, -y, 'tab:red')
+# axs[1, 1].set_title('Axis [1, 1]')
+
+# plt.plot(t_noise, noise_chan.data, label="Inverter On 0 m/s")
+# plt.plot(t_clean, clean_signal, label="Inverter Off 0 m/s")
+# plt
+# plt
+# plt.legend()
+plt.show()
+
+# # %%
